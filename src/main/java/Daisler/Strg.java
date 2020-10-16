91b0e3f0c19186a8418641a4f849351c77ed0f8c
Y31w1tz482 <torben.groetzinger@schueler.gs-wt.de>
2020-10-16 13:39:08 +0200

    Added Thread.sleep option for 30 min. break

--- src/main/java/Daisler/Strg.java
+++ src/main/java/Daisler/Strg.java
@@ -8,8 +8,9 @@ import java.nio.file.*;
 
 public class Strg{
 
-    public static void main(String[] args) throws NullPointerException, MalformedURLException, IOException{ 
-
+    public static void main(String[] args) throws NullPointerException, MalformedURLException, IOException, InterruptedException{ 
+        
+        while(true){
         App a = new App(); // Neuer Website Download
         //Path p = Path.of("/home/torbeng/Dokumente/Websites");
 
@@ -22,7 +23,8 @@ public class Strg{
             System.out.println("STRG-Fehler");
 
         }
-        
+        Thread.sleep(30* 60 * 1000);
+    }
     ///home/torbeng/Dokumente
     //String fs = System.getProperty("file.separator");
     //System.out.print(fs);
