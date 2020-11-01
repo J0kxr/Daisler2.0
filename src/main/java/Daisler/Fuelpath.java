c7a74d9f529e2238ec3042d3a7b9919732d7b233
Y31w1tz482 <torben.groetzinger@schueler.gs-wt.de>
2020-11-01 01:39:55 +0100

    add class to create specific day files and paths

---
+++ src/main/java/Daisler/Fuelpath.java
@@ -0,0 +1,78 @@
+package Daisler;
+
+import java.util.Date;
+import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.*;
+import java.nio.file.Files;
+import java.nio.file.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Fuelpath{
+
+//private String pathToFuel; //"/home/torbeng/Dokumente/Websites"; 
+private String actualDatePath;
+
+//setactualDatePath(getDate()); 
+/*
+public Fuelpath(){
+    return;
+}
+*/
+private static String getDate(){
+    Date d = new Date();
+    String dateInString = d.toString();
+
+    String dayMonth = dateInString.substring(4, 10);
+    String dayMonthPath = "/" + dayMonth;  
+    return dayMonthPath;
+}
+
+
+
+public String getFullPath(String fuelType)throws IOException{
+    String DatePath = null;
+    String newDatePath;
+    
+    newDatePath = getDate();//setNewDatePath(getDate());
+
+    String fullNewPath;
+    while(newDatePath.equals(DatePath)){
+        
+        if (fuelType == "Diesel"){
+            return "/home/torbeng/Dokumente/Websites/Diesel" + newDatePath; 
+    
+        }else if (fuelType == "Benzin"){
+            return "/home/torbeng/Dokumente/Websites/Benzin" + newDatePath;
+    
+        }else {
+            System.out.println("Keine Datensammlung für " + fuelType + "!");    
+        }
+    }
+
+    if (fuelType == "Diesel"){ 
+        Path path = Paths.get("/home/torbeng/Dokumente/Websites/Diesel/" + newDatePath);     
+        Files.createDirectories(path);
+
+        fullNewPath = "/home/torbeng/Dokumente/Websites/Diesel" + newDatePath;
+        return fullNewPath; 
+
+    }else if (fuelType == "Benzin"){
+        Path path = Paths.get("/home/torbeng/Dokumente/Websites/Benzin" + newDatePath);
+        Files.createDirectories(path);
+        
+        fullNewPath = "/home/torbeng/Dokumente/Websites/Benzin" + newDatePath;
+        return fullNewPath;
+
+    }else {
+        System.out.println("Keine Datensammlung für " + fuelType + "!");    
+        } 
+
+        DatePath = newDatePath;
+
+        return "/home/torbeng/Dokumente/Websites";
+    }
+    
+}
