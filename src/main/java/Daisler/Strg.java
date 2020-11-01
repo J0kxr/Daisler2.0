c505ad6bafae5e5594f8e5d7c8467a109d6174f9
Y31w1tz482 <torben.groetzinger@schueler.gs-wt.de>
2020-11-01 01:37:49 +0100

    changed savefile attribute for path to fuelpath call

--- src/main/java/Daisler/Strg.java
+++ src/main/java/Daisler/Strg.java
@@ -1,6 +1,7 @@
 package Daisler; 
 
 import java.io.IOException;
+import java.io.*;
 import java.net.*;
 import java.nio.*;
 import java.nio.file.*;
@@ -8,24 +9,32 @@ import java.nio.file.*;
 
 public class Strg{
 
-    public static void main(String[] args) throws NullPointerException, MalformedURLException, IOException, InterruptedException{ 
-        
-        while(true){
-        App a = new App(); // Neuer Website Download
-        //Path p = Path.of("/home/torbeng/Dokumente/Websites");
-        App d = new App();//new Obejct for Diesel
+    public static void main(String[] args) throws NullPointerException, MalformedURLException, IOException, InterruptedException, NoSuchFileException{ 
+        int a = 0;
+
+        while(a == 0){
+        Fuelpath f = new Fuelpath();
+        //Path f = Path.of("/home/torbeng/Dokumente/Websites");
+
+        App benzin = new App(); // Neuer Website Download
+        App diesel = new App();//new Obejct for Diesel
+           
+        System.out.println("Benzinpfad:" + f.getFullPath("Benzin"));
+        System.out.println("Dieselpfad:" + f.getFullPath("Diesel"));
+        System.out.println(" | ");
 
 
         try {
-            a.SaveFile(new URL("https://www.benzinpreis-aktuell.de/benzinpreise-waldshuttiengen-heute") , "/home/torbeng/Dokumente/Websites"); //save text in file//Benzin
-            d.Savefile(new URL(URL), pathToDiesel);
+            benzin.SaveFile(new URL("https://www.benzinpreis-aktuell.de/benzinpreise-waldshuttiengen-heute") , f.getFullPath("Benzin")); //f.getFullPath("Benzin")save text in file//Benzin
+            diesel.SaveFile(new URL("https://www.benzinpreis-aktuell.de/dieselpreise-waldshuttiengen-heute"), f.getFullPath("Diesel"));
 
         }catch (NullPointerException e) {
             e.printStackTrace();
             System.out.println("STRG-Fehler");
 
         }
-        Thread.sleep(30* 60 * 1000);
+        //Thread.sleep(30* 60 * 1000);
+        ++a;
     }
     ///home/torbeng/Dokumente
     //String fs = System.getProperty("file.separator");
